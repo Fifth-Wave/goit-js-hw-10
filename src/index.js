c5e57fbe18fa7b99be833de5b9a69dd397d01de4
Dmytro Yeskov <wavefifth@gmail.com>
2022-02-07 22:22:52 +0200

    update task 10

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
 import './css/styles.css';
 import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
 
-const DEBOUNCE_DELAY = 300;
+const DEBOUNCE_DELAY = 400;
 const elements = {
   input: document.querySelector('#search-box'),
 };
-
+// debounce(onInput, DEBOUNCE_DELAY);
 elements.input.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
 
-const onInput = function (evt) {
-  const link = '';
-  fetch(link);
-};
+function onInput(evt) {
+  const inputValue = evt.target.value.trim();
+
+  const link = `https://restcountries.com/v3.1/name/${inputValue}`;
+  if (inputValue) {
+    fetch(link)
+      .then(res => res.json())
+      .then(console.log)
+      .catch(Notiflix.Notify.failure('Oops, there is no contry with that name'));
+  }
+}
