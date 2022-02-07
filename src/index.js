cd729deb2fd0c945f9be2184501f077aa495efa4
Dmytro Yeskov <wavefifth@gmail.com>
2022-02-07 20:49:57 +0200

    task initial commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-import './sass/main.scss';
+import './css/styles.css';
+import debounce from 'lodash.debounce';
+
+const DEBOUNCE_DELAY = 300;
+const elements = {
+  input: document.querySelector('#search-box'),
+};
+
+elements.input.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
+
+const onInput = function (evt) {
+  const link = '';
+  fetch(link);
+};
