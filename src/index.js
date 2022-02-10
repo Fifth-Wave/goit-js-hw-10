27ea23e0611ace59d2343fc836fdac74710e036c
Dmytro Yeskov <88843350+Fifth-Wave@users.noreply.github.com>
2022-02-10 11:46:59 +0200

    add templates

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import './sass/country-list.scss';
 import debounce from 'lodash.debounce';
 import { Notify } from 'notiflix';
 import { fetchCountries } from './js/fetchCountries';
+import singlCountryMarkup from './js/singlCountyMarkup.hbs';
+import countryItemMarkup from './js/countryItemMarkup.hbs';
 
 const DEBOUNCE_DELAY = 400;
 const element = {
@@ -43,35 +45,20 @@ function countryRender(res) {
   if (res.length === 1) {
     return res
       .map(({ flags, name, capital, languages, population }) => {
-        return `
-       <li class="country-item">
-          <div class="country">
-            <img class="flag" src="${flags.svg}" alt="" width="40px">
-            <div class="country-name" style="font-size: 40px; font-weight: bold">${
-              name.official
-            }</div>
-          </div>
-          <div class="capital"><span>Capital: </span>${capital.join(',')}</div>
-          <div class="population"><span>Population: </span>${population}</div>
-          <div class="language"><span>Language: </span>${Object.values(languages).join(', ')}</div>
-        </li>
-    `;
+        return singlCountryMarkup({
+          flag: flags.svg,
+          name: name.official,
+          capital: capital.join(','),
+          population,
+          languages: Object.values(languages).join(', '),
+        });
       })
       .join('');
   }
 
   if (res.length >= 2 && res.length <= 10) {
     return res
-      .map(({ flags, name }) => {
-        return `
-       <li class="country-item">
-          <div class="country">
-            <img class="flag" src="${flags.svg}" alt="" width="40px">
-            <div class="country-name">${name.official}</div>
-          </div>
-        </li>
-    `;
-      })
+      .map(({ flags, name }) => countryItemMarkup({ flag: flags.svg, name: name.official }))
       .join('');
   }
 }
